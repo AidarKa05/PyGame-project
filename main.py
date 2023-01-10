a9d80609ae7a002d3d3ed917b9994fa3045095b1
Aidar <aidark054@gmail.com>
2023-01-10 23:30:57 +0300

    Global commit 10.01

--- main.py
+++ main.py
@@ -1,200 +1,17 @@
+import time
+
 import pygame
 import sys
-import os
+from methods_lib import start_screen, load_level, generate_level, all_sprites, screen, menu_screen
 
 FPS = 60
 clock = pygame.time.Clock()
-
-size = WIDTH, HEIGHT = 1400, 800
-
-pygame.init()
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Labyrinth of freedom')
-pygame.display.set_icon(pygame.image.load('data/ico.jpg'))
-
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-walls_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
-finish_group = pygame.sprite.Group()
-
-
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
-
-
-def start_screen():
-    intro_text = ["", "", "", "", "", "Чтобы продолжить нажмите на любую кнопку"]
-    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
-    screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 40)
-    text_coord = 0
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('white'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 100
-        intro_rect.top = text_coord
-        intro_rect.x = 350
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                return
-        pygame.display.flip()
-        clock.tick(FPS)
-
-
-def load_level(filename):
-    filename = "data/" + filename
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
-    max_width = max(map(len, level_map))
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-
-
-tile_images = {
-    'gr': load_image('gr.jpg'),
-    'wwr': load_image('wwr.jpg'),
-    'up_l': load_image('wl.jpg'),
-    'up_r': load_image('wr.jpg'),
-    'dw_l': load_image('nl.jpg'),
-    'dw_r': load_image('nr.jpg'),
-    'finish': load_image('finish.jpg'),
-    'wall': load_image('wall.jpg'),
-    'fon': load_image('fon_map.jpg'),
-    'empty': load_image('block.jpg')
-}
-player_image = load_image('npl.png', -1)
-
-tile_width = tile_height = 50
-
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
-
-
-class Wall(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(walls_group, all_sprites)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
-        self.x = pos_x
-        self.y = pos_y
-
-
-class Finish(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(finish_group, all_sprites)
-        self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__(player_group, all_sprites)
-        self.image = player_image
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
-        self.speedx = 0
-        self.speedy = 0
-        self.rect.x = tile_width * pos_x
-        self.rect.y = tile_height * pos_y
-        self.coins = None
-        self.count_coins = 0
-        self.f = False
-        self.fin = True
-        self.move = True
-
-    def update(self):
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
-        if pygame.sprite.spritecollideany(self, walls_group):
-            self.move = True
-            if self.speedx > 0:
-                self.speedx = 0
-                self.rect.x = self.rect.x - 10
-            if self.speedx < 0:
-                self.speedx = 0
-                self.rect.x = self.rect.x + 10
-            if self.speedy > 0:
-                self.speedy = 0
-                self.rect.y = self.rect.y - 10
-            if self.speedy < 0:
-                self.speedy = 0
-                self.rect.y = self.rect.y + 10
-        collisions = pygame.sprite.groupcollide(player_group, tiles_group, False, True)
-        if pygame.sprite.spritecollideany(self, finish_group) and self.fin:
-            self.speedx = 0
-            self.speedy = 0
-            self.f = True
-            self.fin = False
-        if self.f:
-            self.rect.x = self.rect.x - 40
-            self.f = False
-
-
-
-
-def generate_level(level):
-    new_player, x, y = None, None, None
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '@':
-                Tile('fon', x, y)
-                new_player = Player(x, y)
-            if level[y][x] == '.':
-                Tile('empty', x, y)
-            elif level[y][x] == '#':
-                Wall('wall', x, y)
-            elif level[y][x] == '0':
-                Tile('fon', x, y)
-            elif level[y][x] == 'f':
-                Finish('finish', x, y)
-            elif level[y][x] == '2':
-                Wall('up_l', x, y)
-            elif level[y][x] == '1':
-                Wall('dw_l', x, y)
-            elif level[y][x] == '3':
-                Wall('up_r', x, y)
-            elif level[y][x] == '4':
-                Wall('dw_r', x, y)
-            elif level[y][x] == '-':
-                Wall('gr', x, y)
-            elif level[y][x] == '|':
-                Wall('wwr', x, y)
-    return new_player, x, y
 
 
 if __name__ == '__main__':
     player = None
     start_screen()
-    player, level_x, level_y = generate_level(load_level('map.txt'))
+    player = generate_level(load_level('map.txt'))
     running = True
     while running:
         for event in pygame.event.get():
@@ -202,26 +19,30 @@ if __name__ == '__main__':
                 running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT and player.move:
-                    player.speedx = 10
+                    player.speedx = 40
                     player.speedy = 0
                     player.move = False
                 if event.key == pygame.K_LEFT and player.move:
-                    player.speedx = -10
+                    player.speedx = -40
                     player.speedy = 0
                     player.move = False
                 if event.key == pygame.K_UP and player.move:
-                    player.speedy = -10
+                    player.speedy = -40
                     player.speedx = 0
                     player.move = False
                 if event.key == pygame.K_DOWN and player.move:
-                    player.speedy = 10
+                    player.speedy = 40
                     player.speedx = 0
                     player.move = False
+        if player.f:
+            time.sleep(1)
+            player.open_menu()
         screen.fill(pygame.Color('black'))
         all_sprites.draw(screen)
         all_sprites.update()
         clock.tick(FPS)
         pygame.display.update()
         pygame.display.flip()
+    pygame.display.quit()
     pygame.quit()
-
+    sys.exit()
---
+++ methods_lib.py
@@ -0,0 +1,233 @@
+import time
+import pygame
+import sys
+import os
+
+
+FPS = 60
+clock = pygame.time.Clock()
+
+size = WIDTH, HEIGHT = 1200, 800
+
+pygame.init()
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Labyrinth of freedom')
+pygame.display.set_icon(pygame.image.load('data/ico.jpg'))
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def start_screen():
+    intro_text = "Чтобы продолжить нажмите на любую кнопку"
+    fon = pygame.transform.scale(load_image('fon.jpg'), (1200, 800))
+    screen.blit(fon, (0, 0))
+    font_name = pygame.font.match_font('arial')
+    font = pygame.font.Font(font_name, 40)
+    text_surface = font.render(intro_text, True, pygame.Color('yellow'))
+    intro_rect = text_surface.get_rect()
+    intro_rect.midtop = (600, 726)
+    screen.blit(text_surface, intro_rect)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.display.quit()
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+tile_width = tile_height = 40
+
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+coins_group = pygame.sprite.Group()
+checkpoint_group = pygame.sprite.Group()
+walls_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+finish_group = pygame.sprite.Group()
+
+tile_images = {
+    'gr': load_image('gr.jpg'),
+    'wwr': load_image('wwr.jpg'),
+    'up_l': load_image('wl.jpg'),
+    'up_r': load_image('wr.jpg'),
+    'dw_l': load_image('nl.jpg'),
+    'dw_r': load_image('nr.jpg'),
+    'finish': load_image('finish.jpg'),
+    'fon': load_image('fon_map.jpg'),
+    'empty': load_image('block.jpg'),
+    'coin': load_image('coin.png', -1)
+}
+
+player_image = load_image('pl1.jpg', -1)
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '0'), level_map))
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Coins(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(coins_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Checkpoint(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(checkpoint_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(walls_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+        self.x = pos_x
+        self.y = pos_y
+
+
+class Finish(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(finish_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+        self.speedx = 0
+        self.speedy = 0
+        self.rect.x = tile_width * pos_x
+        self.rect.y = tile_height * pos_y
+        self.count_coins = 0
+        self.f = False
+        self.move = True
+
+    def update(self):
+        score = str(self.count_coins)
+        font = pygame.font.Font(None, 30)
+        text_surface = font.render(score, True, pygame.Color('yellow'))
+        intro_rect = text_surface.get_rect()
+        intro_rect.midtop = (40, 40)
+        screen.blit(text_surface, intro_rect)
+
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if pygame.sprite.spritecollideany(self, walls_group):
+            self.move = True
+            if self.speedx > 0:
+                self.speedx = 0
+                self.rect.x = self.rect.x - 40
+            if self.speedx < 0:
+                self.speedx = 0
+                self.rect.x = self.rect.x + 40
+            if self.speedy > 0:
+                self.speedy = 0
+                self.rect.y = self.rect.y - 40
+            if self.speedy < 0:
+                self.speedy = 0
+                self.rect.y = self.rect.y + 40
+        collis_coin = pygame.sprite.groupcollide(player_group, coins_group, False, True)
+        if collis_coin:
+            for coin in collis_coin.values():
+                self.count_coins += 50 * len(coin)
+        collis_check = pygame.sprite.groupcollide(player_group, checkpoint_group, False, True)
+        if collis_check:
+            for check in collis_check.values():
+                self.count_coins += 10 * len(check)
+        if pygame.sprite.spritecollideany(self, finish_group):
+            self.f = True
+
+    def open_menu(self):
+        menu_screen()
+
+
+def generate_level(level):
+    new_player = None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '@':
+                Tile('fon', x, y)
+                new_player = Player(x, y)
+            if level[y][x] == '.':
+                Checkpoint('empty', x, y)
+            elif level[y][x] == '0':
+                Tile('fon', x, y)
+            elif level[y][x] == 'f':
+                Finish('finish', x, y)
+            elif level[y][x] == '2':
+                Wall('up_l', x, y)
+            elif level[y][x] == '1':
+                Wall('dw_l', x, y)
+            elif level[y][x] == '3':
+                Wall('up_r', x, y)
+            elif level[y][x] == '4':
+                Wall('dw_r', x, y)
+            elif level[y][x] == '-':
+                Wall('gr', x, y)
+            elif level[y][x] == '|':
+                Wall('wwr', x, y)
+            elif level[y][x] == 'a':
+                Coins('coin', x, y)
+    return new_player
+
+
+def menu_screen():
+    intro_text = "Чтобы продолжить нажмите на любую кнопку"
+    fon = pygame.transform.scale(load_image('menu_fon.jpg'), (1200, 800))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 40)
+    text_surface = font.render(intro_text, True, pygame.Color('yellow'))
+    intro_rect = text_surface.get_rect()
+    intro_rect.midtop = (600, 730)
+    screen.blit(text_surface, intro_rect)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.display.quit()
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
